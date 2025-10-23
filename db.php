fd5aac27cb74d56b7925fe8ed7297f6edb34f063
zohaibzamiraiwala-prog <zohaibzamiraiwala@gmail.com>
2025-10-23 18:38:36 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+// db.php - Database Connection File
+$servername = "localhost"; // Assuming localhost, change if needed
+$username = "uiumzmgo1eg2q";
+$password = "kuqi5gwec3tv";
+$dbname = "dbajz4qfspqnlt";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
